3302bcb360b74f730f161f1bc84000bbdaa41c3e
Raghavendra <raghav1057@gmail.com>
2016-01-28 18:35:34 +0530

    :q :uuuuuuuu

--- rag_projs/rag_projs/urls.py
+++ rag_projs/rag_projs/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    url('^admin/$', admin.site.urls),
+    url('^admin/', include(admin.site.urls)),
     url('^app1/', include('app1.urls')),
     url('^app1$/|^rag$', include('app1.urls')),
 ]
